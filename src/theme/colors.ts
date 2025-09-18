1031dc2c13c0bf418510c0008a9f339470faa140
sonjunho2 <sonjunho2@gmail.com>
2025-09-18 14:01:28 +0900

    Create colors.ts

---
+++ src/theme/colors.ts
@@ -0,0 +1,13 @@
+// src/theme/colors.ts
+export const colors = {
+  brandPrimary: '#FF9800',   // 오렌지
+  brandGold:    '#FFC107',   // 골드
+  bgSoft:       '#FFF8E1',   // 크림 배경
+  textPrimary:  '#212121',
+  textSecondary:'#616161',
+  white:        '#FFFFFF',
+  black:        '#000000',
+};
+export const gradients = {
+  brand: [ '#FFC107', '#FF9800' ],
+};
