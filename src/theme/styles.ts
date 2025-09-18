03d2b15e7d784d9660c6700cc8798546098eb53f
sonjunho2 <sonjunho2@gmail.com>
2025-09-18 16:11:06 +0900

    Create styles.ts

---
+++ src/theme/styles.ts
@@ -0,0 +1,21 @@
+/새파일 /src/theme/styles.ts
+import { StyleSheet } from 'react-native';
+import colors from './colors';
+
+export const commonStyles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: colors.background,
+    padding: 16,
+  },
+  title: {
+    fontSize: 20,
+    fontWeight: '700',
+    color: colors.text,
+    marginBottom: 12,
+  },
+  text: {
+    fontSize: 16,
+    color: colors.text,
+  },
+});
